b442a8ea55617a78c3f4013dd466f72ad9ecc53c
loren <111726817+lorennnzzoo@users.noreply.github.com>
2024-02-29 18:25:19 +0530

    removed versioins for modifying
    while modifying we use the latest json blocks and properties for that so no need to specify it
    and also added ethernet block

--- lynxhelper.py
+++ lynxhelper.py
@@ -13,13 +13,5 @@ elif(versionSelec=="3.91"):
 elif(versionSelec=="4.01"):
     process = subprocess.run(os.getcwd()+"/base-version-files/lynx testing 4.01.json", shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-elif(versionSelec=='F'):
-    selectVersion=input("Please Provide The Version Number For File You Want To Edit ex: default,3.7,3.91,4.01 : ")
-    if(selectVersion=="default"):
-        d.defaultModifier()
-    elif(selectVersion=="3.7"):
-        d.threeSevenModifier()
-    elif(selectVersion=="3.91"):
-        d.threeNineOneModifier()
-    elif(selectVersion=="4.01"):
-        d.fourZeroOneModifier()
+elif(versionSelec=='F'):    
+    d.defaultModifier()    
--- modifieres.py
+++ modifieres.py
@@ -36,7 +36,8 @@ def defaultModifier():
     "dataScale": 1,
     "nwFallback": "0",
     "modBus2": 0,
-    "autoRebootCycle": "0"
+    "autoRebootCycle": "0",
+    "whiteLabel": 1
 }
 
 # Update specific values
@@ -50,72 +51,90 @@ def defaultModifier():
     dotFourSettingsBlock= "\"dotFourSettings\": "+dotfourstring+""
     print('dotFourSettingsBlock Generated')
 
+    print('adding additional settings')
 
-    userInputModbus=input('\n\nPlease Give Inputs For Modbus baudrate and parity For \nExample 9600,1 : ')
-    baudrate=userInputModbus.split(',')[0]
-    parity=userInputModbus.split(',')[1]
-    modBusSettingsBlock = {
-    "mbBaudRate": "9600",
-    "mbParity": "1",
-    "mbSlaveCnt": 1,
-    "mbReadTryCount": "1"
-  }
-    modBusSettingsBlock['mbBaudRate']=baudrate
-    modBusSettingsBlock["mbParity"]=parity
-    modbusSettingsString=json.dumps(modBusSettingsBlock)
-    
-    dotFourSettingsBlock+=", \"modBusSettings\": "+modbusSettingsString+""    
-    print('modbus Settings block is generated')
+    dotFourSettingsBlock+=',\"whiteLabelSettings":{\"deviceName":\"NK Envion",\"line1":\"NK Square",\"line2":\"Envion-Y"}'
+    dotFourSettingsBlock+=',\"nfhSettings":{\"nfhMode":10,\"maxLoopCnt":4,\"nfhDataDir":\"/nfhTmpDir"}'
+    dotFourSettingsBlock+=',\"rtcSettings":{\"bootupNtpSync":1,\"timeZoneMinutes":\"330",\"poolServerName":\"pool.ntp.org"}'
 
-    userInputModbusSlavesAndRegisters=input('\n\nPlease Provide slave id and no of regsters you want to read \nfor example 1,4 : ')
-    slaveId=userInputModbusSlavesAndRegisters.split(',')[0]
-    registersCount=userInputModbusSlavesAndRegisters.split(',')[1]
-    print('You will be asked to enter registers data '+str(registersCount)+' times')
-    mbarray = '['
-    for i in range(int(registersCount)):
-        userInputRegistersData = input('\n\nPlease enter varName,registerNo,dataType,factor \nfor example MODBUS,40004,1,204: ')
-        registerName = userInputRegistersData.split(',')[0]
-        registerNumber = userInputRegistersData.split(',')[1]
-        dataType = userInputRegistersData.split(',')[2]
-        factor = userInputRegistersData.split(',')[3]
+    if int(modbus) is 1:
+        userInputModbus=input('\n\nPlease Give Inputs For Modbus baudrate and parity For \nExample 9600,1 : ')
+        baudrate=userInputModbus.split(',')[0]
+        parity=userInputModbus.split(',')[1]
+        modBusSettingsBlock = {
+        "mbBaudRate": "9600",
+        "mbParity": "1",
+        "mbSlaveCnt": 1,
+        "mbReadTryCount": "1"
+    }
+        modBusSettingsBlock['mbBaudRate']=baudrate
+        modBusSettingsBlock["mbParity"]=parity
+        modbusSettingsString=json.dumps(modBusSettingsBlock)
+        
+        dotFourSettingsBlock+=", \"modBusSettings\": "+modbusSettingsString+""    
+        print('modbus Settings block is generated')
 
-        if i > 0:
-            mbarray += ',{"varName":"' + str(registerName) + '","mbReg":"' + str(registerNumber) + '","varType":"' + str(dataType) + '","varFactor":"' + str(factor) + '"}'
+        userInputModbusSlavesAndRegisters=input('\n\nPlease Provide slave id and no of regsters you want to read \nfor example 1,4 : ')
+        slaveId=userInputModbusSlavesAndRegisters.split(',')[0]
+        registersCount=userInputModbusSlavesAndRegisters.split(',')[1]
+        print('You will be asked to enter registers data '+str(registersCount)+' times')
+        mbarray = '['
+        for i in range(int(registersCount)):
+            userInputRegistersData = input('\n\nPlease enter varName,registerNo,dataType,factor \nfor example MODBUS,40004,1,204: ')
+            registerName = userInputRegistersData.split(',')[0]
+            registerNumber = userInputRegistersData.split(',')[1]
+            dataType = userInputRegistersData.split(',')[2]
+            factor = userInputRegistersData.split(',')[3]
+
+            if i > 0:
+                mbarray += ',{"varName":"' + str(registerName) + '","mbReg":"' + str(registerNumber) + '","varType":"' + str(dataType) + '","varFactor":"' + str(factor) + '"}'
+            else:
+                mbarray += '{"varName":"' + str(registerName) + '","mbReg":"' + str(registerNumber) + '","varType":"' + str(dataType) + '","varFactor":"' + str(factor) + '"}'
+
+        mbarray += ']'
+
+        slaveAndRegistersBlock ='{"slaveID": "'+slaveId+'","mbRegCnt": '+parity+',"mbArray": '+mbarray+'}'
+        dotFourSettingsBlock+=',"modBusSlaves":['+str(slaveAndRegistersBlock)+']'                    
+        print('\n\nregister data and slave data block is generated')
+    if int(analog) is 1:
+
+        userInputAnalogSettings=input('\n\nPlease enter the no of analog channels and its values count \nfor example 1 to 8 : ')    
+        analogchannels=userInputAnalogSettings
+        print('you will be asked to enter analog channels data for '+analogchannels+' times')
+        anaToDigArray=str()
+        for i in range(int(analogchannels)):
+            userinputAnalogData=input('\n\nPlease enter analog data for channel '+str(i+1)+' varname,varname2,minrange,maxrange,factor\nfor example PM,PM_ma,0,1000,1 : ')        
+            varName=userinputAnalogData.split(',')[0]
+            varName2=userinputAnalogData.split(',')[1]
+            minRange=userinputAnalogData.split(',')[2]
+            maxRange=userinputAnalogData.split(',')[3]
+            factor=userinputAnalogData.split(',')[4]
+            if i>0:
+                anaToDigArray +=',{\"anaToDigType":1,\"minOutVal":0,\"maxOutVal":20,\"minmA":0,\"maxmA":20,\"varName":\"'+(varName)+'",\"varName2":\"'+(varName2)+'",\"varFactor":'+factor+'}'
+            else:
+                anaToDigArray +='{\"anaToDigType":1,\"minOutVal":0,\"maxOutVal":20,\"minmA":0,\"maxmA":20,\"varName":\"'+(varName)+'",\"varName2":\"'+(varName2)+'",\"varFactor":'+factor+'}'
+        analogSettingsBlock=',\"anaToDigSettings":{ \"anaToDigCnt":'+analogchannels+',\"anaToDigArray":['+str(anaToDigArray)+']}'
+        
+
+        dotFourSettingsBlock+=analogSettingsBlock
+        print('\n\n analog settings block generated')
+
+    if int(ethernet)  is 1:
+        userInputEtherNetSettings=input('\n\nPlease enter IP,SUBNETMASK,GATEWAY,DNS \nFor example 192.168.1.243,255.255.255.0,192.168.1.1,8.8.8.8  if not needed enter F : ')
+        if(userInputEtherNetSettings=='F'):
+            dotFourSettingsBlock+=',\"ethernetSettings":{\"userStaticIP":[\"192",\"168",\"1,\"143"],\"etherSubnet":[\"255",\"255",\"255",\"0"],\"etherGateway":[\"192",\"168",\"1",\"1"],\"etherDns":[\"8",\"8",\"8",\"8]}'
         else:
-            mbarray += '{"varName":"' + str(registerName) + '","mbReg":"' + str(registerNumber) + '","varType":"' + str(dataType) + '","varFactor":"' + str(factor) + '"}'
-
-    mbarray += ']'
-
-    slaveAndRegistersBlock ='{"slaveID": "'+slaveId+'","mbRegCnt": '+parity+',"mbArray": '+mbarray+'}'
-    dotFourSettingsBlock+=',"modBusSlaves":['+str(slaveAndRegistersBlock)+']'                    
-    print('\n\nregister data and slave data block is generated')
-
-    userInputAnalogSettings=input('\n\nPlease enter the no of analog channels and its values count \nfor example : 1to8')    
-    analogchannels=userInputAnalogSettings
-    print('you will be asked to enter analog channels data for '+analogchannels+' times')
-    for i in range(int(analogchannels)):
-        userinputAnalogData=input('Please enter analog data for channel '+str(i+1)+' varname,varname2,minrange,maxrange,factor\nfor example PM,PM_ma,0,1000,1 : ')        
-        varName=userinputAnalogData.split(',')[0]
-        varName2=userinputAnalogData.split(',')[1]
-        minRange=userinputAnalogData.split(',')[2]
-        maxRange=userinputAnalogData.split(',')[3]
-        factor=userinputAnalogData.split(',')[4]
-        if(i>0):
-            anaToDigArray=',{\"anaToDigType":1,\"minOutVal":0,\"maxOutVal":20,\"minmA":0,\"maxmA":20,\"varName":\"'+(varName)+'",\"varName2":\"'+(varName2)+'",\"varFactor":'+factor+'}'
-        else:
-            anaToDigArray='{\"anaToDigType":1,\"minOutVal":0,\"maxOutVal":20,\"minmA":0,\"maxmA":20,\"varName":\"'+(varName)+'",\"varName2":\"'+(varName2)+'",\"varFactor":'+factor+'}'
-    analogSettingsBlock=',\"anaToDigSettings":{ \"anaToDigCnt":'+analogchannels+',\"anaToDigArray":['+str(anaToDigArray)+']}'
-    print('\n\n analog settings block generated')
-
-    dotFourSettingsBlock+=analogSettingsBlock
-
+            ip=userInputEtherNetSettings.split(',')[0]
+            subnet=userInputEtherNetSettings.split(',')[1]
+            gateway=userInputEtherNetSettings.split(',')[2]
+            dns=userInputEtherNetSettings.split(',')[3]
+            ipArray=ip.split('.')
+            subnetArray=subnet.split('.')
+            gatewayArray=gateway.split('.')
+            dnsArray=dns.split('.')
+            dotFourSettingsBlock+=',\"ethernetSettings":{\"userStaticIP":[\"'+ipArray[0]+'",\"'+ipArray[1]+'",\"'+ipArray[2]+'",\"'+ipArray[3]+'"],\"etherSubnet":[\"'+subnetArray[0]+'",\"'+subnetArray[1]+'",\"'+subnetArray[2]+'",\"'+subnetArray[3]+'"],\"etherGateway":[\"'+gatewayArray[0]+'",\"'+gatewayArray[1]+'",\"'+gatewayArray[2]+'",\"'+gatewayArray[3]+'"],\"etherDns":[\"'+dnsArray[0]+'",\"'+dnsArray[1]+'",\"'+dnsArray[2]+'",\"'+dnsArray[3]+'"]}'
+            print('\n\nEthernet Settings Block Generated')
 
     print('\n\n '+dotFourSettingsBlock)
     json_data = json.loads('{'+dotFourSettingsBlock+'}')
     print(json_data)
-def threeSevenModifier():
-    pass
-def threeNineOneModifier():
-    pass
-def fourZeroOneModifier():
-    pass
